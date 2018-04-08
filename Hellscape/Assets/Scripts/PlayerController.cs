777328de9f66487eb53277ba1c07fab1fcd2f439
VicAlexe <37591060+VicAlexe@users.noreply.github.com>
2018-04-08 11:24:06 +0300

    This is working

---
+++ Hellscape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /*
+       w and s control speed 
+       mouse controls potential turn
+       a and d control strafing
+    */
+
+    
+    private float speed, turn, strafe;
+
+    [SerializeField]
+    private Animator anim;
+
+    #region
+
+    private void Update()
+    {
+        ControlSpeed();
+        ControlTurn();
+       // ControlStrafe();
+    }
+
+    private void ControlSpeed()
+    {
+        
+        speed = Input.GetAxis("Vertical");
+        anim.SetFloat("Speed", speed);
+      
+    }
+
+    private void ControlTurn()
+    {
+        turn = Input.GetAxis("Horizontal");
+        anim.SetFloat("Turn", turn);
+    }
+
+    private void ControlStrafe()
+    {
+        
+    }
+
+    #endregion
+}
